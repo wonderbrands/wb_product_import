c6d7145f769bdfd6f9494a7424c6787c30964f88
AnibalArenas <anibal@wonderbrands.co>
2022-09-21 18:49:44 -0500

    [UPD] modifica la dirección web del manifest

--- wb_product_import/__manifest__.py
+++ wb_product_import/__manifest__.py
@@ -23,8 +23,8 @@
         Honorarios Agente Aduanal
     """,
 
-    'author': "Wonder Brands",
-    'website': "http://www.wonderbrands.com",
+    'author': "Wonderbrands",
+    'website': "https://www.wonderbrands.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
