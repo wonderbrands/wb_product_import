3c5e67918b486aa155fd293a4e6fe15f9808e898
demianavilar@gmail.com <Demian Avila>
2025-03-21 01:39:05 +0100

    fix: modified to be installed in v18

--- __manifest__.py
+++ __manifest__.py
@@ -30,7 +30,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '15.0',
+    'version': '18.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','product','purchase'],
@@ -41,6 +41,5 @@
         #'security/security.xml',
         'data/import.noms.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
 }
